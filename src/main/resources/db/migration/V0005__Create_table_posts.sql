81e68a514c0fef41cfda00265b0119b9ec142e98
script.sh <script.sh.me@gmail.com>
2023-02-20 17:27:38 +0200

    edit sql migration script for creating table posts

--- src/main/resources/db/migration/V0005__Create_table_posts.sql
+++ src/main/resources/db/migration/V0005__Create_table_posts.sql
@@ -4,7 +4,7 @@ CREATE TABLE posts
     user_id     INT NOT NULL references users,
     title       CHARACTER VARYING(60) NOT NULL,
     content     TEXT NOT NULL,
-    publish_date INT NOT NULL
+    publish_date BIGINT NOT NULL
 );
 
 create unique index posts_title_index on posts (title);
